ee93da42dd48c5d10849091e9e5d3684a56a037f
KojiKobayashi <k.kobayashi@sansan.com>
2026-02-08 17:06:33 +0900

    fix: コミット漏れ

---
+++ src/settings.py
@@ -0,0 +1,5 @@
+cell_height, cell_width = 27, 33 # セルの幅、高さ
+line_thickness = 1  # 通常の区切り線の太さ
+thick_line_thickness = 3    # 太い区切り線の太さ
+thick_line_interval = 5  # 太い区切り線を入れる間隔（セル数）
+no_index_color = [0, 0, 0]  # インデックスが存在しない場合のデフォルトカラー
